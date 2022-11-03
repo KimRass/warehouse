3fac33ad806e72231f168ec9ba34b06f81bba10e
KimRass <rmx1000@yonsei.ac.kr>
2022-11-03 18:54:02 +0900

    feat: fastapi

--- python/fastapi/main.py
+++ python/fastapi/main.py
@@ -56,3 +56,9 @@ async def get_model(model_name: ModelName):
         return {"model_name": model_name, "message": "LeCNN all the images"}
 
     return {"model_name": model_name, "message": "Have some residuals"}
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
