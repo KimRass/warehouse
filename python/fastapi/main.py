3ddb9931cc707fc596bc44be3e185b6bc210c903
KimRass <rmx1000@yonsei.ac.kr>
2022-11-09 19:00:31 +0900

    fastapi

--- python/fastapi/main.py
+++ python/fastapi/main.py
@@ -1,6 +1,6 @@
 # Reference: https://fastapi.tiangolo.com/ko/tutorial/
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from enum import Enum
 
 app = FastAPI()
@@ -62,3 +62,45 @@ fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"
 @app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 10):
     return fake_items_db[skip: skip + limit]
+
+# When you need to send data from a client (let's say, a browser) to your API, you send it as a request body.
+# A request body is data sent by the client to your API. A response body is the data your API sends to the client.
+# Your API almost always has to send a response body. But clients don't necessarily need to send request bodies all the time.
+
+from typing import List, Union
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
+
+app = FastAPI()
+
+
+# @app.post("/items/")
+# async def create_item(item: Item):
+#     item_dict = item.dict()
+#     if item.tax:
+#         price_with_tax = item.price + item.tax
+#         item_dict.update({"price_with_tax": price_with_tax})
+#     return item_dict
+
+
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.dict()}
+
+# The function parameters will be recognized as follows:
+    # If the parameter is also declared in the path, it will be used as a path parameter.
+    # If the parameter is of a singular type (like `int`, `float`, `str`, `bool`, etc) it will be interpreted as a query parameter.
+    # If the parameter is declared to be of the type of a Pydantic model, it will be interpreted as a request body.
+
+@app.get("/items/")
+async def read_items(q: Union[List[str], None] = Query(default=None)):
+    query_items = {"q": q}
+    return query_items
+
