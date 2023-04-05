a80d0c9de01701399e198d5bbfd26a70acb5e698
KimRass <rmx1000@yonsei.ac.kr>
2023-04-05 19:04:35 +0900

    Minor changes

--- computer_vision/image_data_augmentation/cutout/cutout.py
+++ computer_vision/image_data_augmentation/cutout/cutout.py
@@ -10,7 +10,7 @@ import numpy as np
 from pathlib import Path
 
 
-def apply_cutout(image, cutout_size=16, mean_rgb=(0, 0, 0)):
+def apply_cutout(image, cutout_size=16, mean=(0.485, 0.456, 0.406)):
     b, _, h, w = image.shape
 
     x = random.randint(0, w)
@@ -20,9 +20,9 @@ def apply_cutout(image, cutout_size=16, mean_rgb=(0, 0, 0)):
     xmax = max(0, x + cutout_size // 2)
     ymax = max(0, y + cutout_size // 2)
 
-    image[:, 0, ymin: ymax, xmin: xmax] = mean_rgb[0]
-    image[:, 1, ymin: ymax, xmin: xmax] = mean_rgb[1]
-    image[:, 2, ymin: ymax, xmin: xmax] = mean_rgb[2]
+    image[:, 0, ymin: ymax, xmin: xmax] = mean[0]
+    image[:, 1, ymin: ymax, xmin: xmax] = mean[1]
+    image[:, 2, ymin: ymax, xmin: xmax] = mean[2]
     return image
 
 
