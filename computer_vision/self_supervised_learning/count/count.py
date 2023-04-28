2557cd351b898081e6d3d515229a35f4f9bb3650
KimRass <rmx1000@yonsei.ac.kr>
2023-04-28 18:06:54 +0900

    Count PyTorch loss function

--- computer_vision/self_supervised_learning/count/count.py
+++ computer_vision/self_supervised_learning/count/count.py
@@ -7,6 +7,7 @@ import torch.nn.functional as F
 import torchvision
 import torchvision.transforms as T
 from torchvision.models import alexnet
+from itertools import combinations
 
 
 class Network(nn.Module):
@@ -36,27 +37,61 @@ class Network(nn.Module):
         # x = self.dropout(x)
         return x
 
+
+class ContrastiveLoss(nn.Module):
+    def __init__(self, model, img_size=228, M=10):
+        super().__init__()
+
+        self.img_size = img_size
+        self.M = M
+        cell_size = self.img_size // 2
+        model = model
+
+    def forward(self, x, y):
+        model = Network()
+        data = torch.randn((batch_size, 3, img_size, img_size))
+        # data = torch.clamp(data, 0, 10)
+        # data *= 2
+        combs = list(combinations(range(batch_size), 2))
+        x = data[[i[0] for i in combs]]
+        y = data[[i[1] for i in combs]]
+
+        cell1 = x[:, :, : cell_size, : cell_size]
+        cell2 = x[:, :, cell_size:, : cell_size]
+        cell3 = x[:, :, : cell_size, cell_size:]
+        cell4 = x[:, :, cell_size:, cell_size:]
+        resized_x = T.Resize((cell_size, cell_size))(x)
+        resized_y = T.Resize((cell_size, cell_size))(y)
+
+        cell1_feat = model(cell1)
+        cell2_feat = model(cell2)
+        cell3_feat = model(cell3)
+        cell4_feat = model(cell4)
+        resized_x_feat = model(resized_x)
+        resized_y_feat = model(resized_y)
+
+        summed_feat = (cell1_feat + cell2_feat + cell3_feat + cell4_feat)
+        loss1 = F.mse_loss(resized_x_feat, summed_feat)
+        # loss1[:, 0]
+        # loss1[:, 1]
+        # loss1[0]
+        loss2 = max(0, self.M - F.mse_loss(resized_y_feat, summed_feat))
+        loss1, loss2
+        # loss = F.mse_loss(resized_x_feat, summed_feat) + max(0, self.M - F.mse_loss(resized_y_feat, summed_feat))
+        # return loss
+        return loss1, loss2
+
 model = Network()
-x = torch.randn((8, 3, 114, 114))
-out = model(x)
-out.shape
+criterion = ContrastiveLoss(model=model)
+img_size=228
+batch_size=8
+# x = torch.randn((1, 3, img_size, img_size))
+# y = torch.randn((1, 3, img_size, img_size))
+# contrastive_loss(x, y)
 
-
-
-img_size = 228
-x = torch.randn((1, 3, img_size, img_size))
-y = torch.randn((1, 3, img_size, img_size))
-
-T1x = x[:, :, : img_size // 2, : img_size // 2]
-T2x = x[:, :, img_size // 2:, : img_size // 2]
-T3x = x[:, :, : img_size // 2, img_size // 2:]
-T4x = x[:, :, img_size // 2:, img_size // 2:]
-Dx = T.Resize((img_size // 2, img_size // 2))(x)
-Dy = T.Resize((img_size // 2, img_size // 2))(y)
-
-phi_T1x = model(T1x)
-phi_T2x = model(T2x)
-phi_T3x = model(T3x)
-phi_T4x = model(T4x)
-phi_Dx = model(Dx)
-phi_Dy = model(Dy)
+data = torch.randn((batch_size, 3, img_size, img_size))
+combs = list(combinations(range(batch_size), 2))
+x = data[[i[0] for i in combs]]
+y = data[[i[1] for i in combs]]
+# x[:, :, : cell_size, : cell_size].shape
+criterion(x, y)
