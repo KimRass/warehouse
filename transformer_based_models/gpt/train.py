eec54a51db0b3b01c6762e9266c7653b2847b168
KimRass <rmx1000@yonsei.ac.kr>
2024-01-30 01:03:03 +0900

    LR scheduler

--- transformer_based_models/gpt/train.py
+++ transformer_based_models/gpt/train.py
@@ -11,17 +11,21 @@ import matplotlib.pyplot as plt
 from image_utils import _figure_to_array, show_image
 
 
-def get_lr_schedule(scheduler, n_steps):
+def get_lr_schedule(optim, scheduler, n_epochs, data_size, batch_size):
+    n_steps_per_epoch = data_size // batch_size
     lrs = list()
-    for step in range(1, n_steps + 1):
-        lrs.append(optimizer.param_groups[0]["lr"])
-        # Should be called after each optimizer update with the index of the next update.
-        scheduler.step_update(num_updates=step + 1)
+    for epoch in range(1, n_epochs + 1):
+        for step in range(1, n_steps + 1):
+            scheduler.step_update(num_updates=(epoch - 1) * n_steps_per_epoch + step + 1)
+        scheduler.step(epoch=epoch + 1)
+
+        lrs.append(optim.param_groups[0]["lr"])
+        print(optim.param_groups[0]["lr"])
     return lrs
 
 
-def visualize_lrs(lrs, n_steps):
-    fig, axes = plt.subplots(figsize=(int(len(lrs) ** 0.2), 3))
+def vis_lrs(lrs, n_steps):
+    fig, axes = plt.subplots(figsize=(8, 3))
     axes.plot(range(1, n_steps + 1), lrs)
     axes.set_ylim([0, max(lrs) * 1.1])
     axes.set_xlim([0, n_steps])
@@ -34,6 +38,18 @@ def visualize_lrs(lrs, n_steps):
     return arr
 
 
+def get_cosine_scheduler(optim, warmup_epochs, n_epochs, init_lr, min_lr):
+    return CosineLRScheduler(
+        optimizer=optim,
+        t_initial=n_epochs,
+        lr_min=min_lr,
+        warmup_t=warmup_epochs,
+        warmup_lr_init=init_lr,
+        warmup_prefix=True,
+        t_in_epochs=True,
+    )
+
+
 if __name__ == "__main__":
     model = torch.nn.Linear(2, 1)
 
@@ -44,17 +60,23 @@ if __name__ == "__main__":
     init_lr = 0
     max_lr = 2.5e-04
     min_lr = 0
-    optimizer = AdamP(model.parameters(), lr=max_lr)
-    scheduler = CosineLRScheduler(
-        optimizer=optimizer,
-        t_initial=n_steps,
-        lr_min=min_lr,
-        warmup_t=2000,
-        warmup_lr_init=init_lr,
-        warmup_prefix=True, # Warmup 부분과 Decay 부분이 자연스럽게 이어집니다.
-        t_in_epochs=False # If `True` the number of iterations is given in terms of epochs
-            # rather than the number of batch updates.
+    # optim = AdamP(model.parameters(), lr=max_lr)
+    # scheduler = CosineLRScheduler(
+    #     optimizer=optim,
+    #     t_initial=n_steps,
+    #     lr_min=min_lr,
+    #     warmup_t=2000,
+    #     warmup_lr_init=init_lr,
+    #     warmup_prefix=True, # Warmup 부분과 Decay 부분이 자연스럽게 이어집니다.
+    #     t_in_epochs=False # If `True` the number of iterations is given in terms of epochs
+    #         # rather than the number of batch updates.
+    # )
+    # lrs = get_lr_schedule(scheduler, n_steps=n_steps)
+    # vis = vis_lrs(lrs=lrs, n_steps=n_steps)
+    optim = AdamP(model.parameters(), lr=0.0005)
+    scheduler = get_cosine_scheduler(
+        optim=optim, warmup_epochs=10, n_epochs=79, init_lr=0, min_lr=0,
     )
-    lrs = get_lr_schedule(scheduler, n_steps=n_steps)
-    vis = visualize_lrs(lrs=lrs, n_steps=n_steps)
+    lrs = get_lr_schedule(optim=optim, scheduler=scheduler, n_epochs=80, data_size=100, batch_size=5)
+    vis = vis_lrs(lrs=lrs, n_steps=80)
     show_image(vis)
