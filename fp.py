a082a4e32c779d532d8e4ee11984d22099be6e4a
KimRass <rmx1000@yonsei.ac.kr>
2024-12-17 23:02:45 +0900

    Floating point

--- fp.py
+++ fp.py
@@ -2,11 +2,11 @@ import math
 
 
 class FPBase(object):
-    def __init__(self, exp_bits, frac_bits):
-        self.exp_bits = exp_bits
-        self.frac_bits = frac_bits
-        self.exp_bias = 2 ** (exp_bits - 1) - 1
-        self.exp_end = self.exp_bits + 1
+    def __init__(self, exponent_bits, fraction_bits):
+        self.exponent_bits = exponent_bits
+        self.fraction_bits = fraction_bits
+        self.exp_bias = 2 ** (exponent_bits - 1) - 1
+        self.exp_end = self.exponent_bits + 1
 
     @staticmethod
     def get_s(num):
@@ -24,7 +24,7 @@ class FPBase(object):
         return M
 
     def __call__(self, num):
-        assert len(num) == 1 + self.exp_bits + self.frac_bits
+        assert len(num) == 1 + self.exponent_bits + self.fraction_bits
 
         E = self.get_E(num)
         M = self.get_M(num)
@@ -33,9 +33,9 @@ class FPBase(object):
                 return 0
             else:
                 return ((-1) ** self.get_s(num)) * M * (
-                    2 ** -(2 ** (self.exp_bits - 1) - 2)
+                    2 ** -(2 ** (self.exponent_bits - 1) - 2)
                 )
-        elif E == 2 ** self.exp_bits - 1:
+        elif E == 2 ** self.exponent_bits - 1:
             if M == 0:
                 return ((-1) ** self.get_s(num)) * math.inf
             else:
@@ -55,39 +55,39 @@ class FP16(FPBase):
     The minimum positive normal values; `fp16("0000010000000001")` (6.109476089477539e-05)
     The maximum representable value: `fp16("0111101111111111")` (65504.0)
     """
-    def __init__(self, exp_bits=5, frac_bits=10):
-        super().__init__(exp_bits=exp_bits, frac_bits=frac_bits)
+    def __init__(self, exponent_bits=5, fraction_bits=10):
+        super().__init__(exponent_bits=exponent_bits, fraction_bits=fraction_bits)
 
 
 class FP32(FPBase):
     """
-    fp32 = FP32()
-    fp32("00000000000000000000000000000000") # 0
+    `fp32 = FP32()`
+    `fp32("00000000000000000000000000000000")` # 0
     # The minimum positive subnormal values; 1.401298464324817e-45
-    fp32("00000000000000000000000000000001")
+    `fp32("00000000000000000000000000000001")`
     # The minimum positive normal values; 1.175494490952134e-38
-    fp32("00000000100000000000000000000001")
+    `fp32("00000000100000000000000000000001")`
     # The maximum representable value; 3.4028234663852886e+38
-    fp32("01111111011111111111111111111111")
-    fp32("01111111100000010000000000000000") # NaN
-    fp32("01111111100000000000000000000000") # Infinity
+    `fp32("01111111011111111111111111111111")`
+    `fp32("01111111100000010000000000000000")` # NaN
+    `fp32("01111111100000000000000000000000")` # Infinity
     """
-    def __init__(self, exp_bits=8, frac_bits=23):
-        super().__init__(exp_bits=exp_bits, frac_bits=frac_bits)
+    def __init__(self, exponent_bits=8, fraction_bits=23):
+        super().__init__(exponent_bits=exponent_bits, fraction_bits=fraction_bits)
 
 
 class FP64(FPBase):
     """
-    fp64 = FP64()
+    `fp64 = FP64()`
     # The minimum positive subnormal values; 5e-324
-    fp64("0000000000000000000000000000000000000000000000000000000000000001")
+    `fp64("0000000000000000000000000000000000000000000000000000000000000001")`
     # The minimum positive normal values; 4.450147717014404e-308
-    fp64("0000000000100000000000000000000000000000000000000000000000000001")
+    `fp64("0000000000100000000000000000000000000000000000000000000000000001")`
     # The maximum representable value; 1.7976931348623157e+308
-    fp64("0111111111101111111111111111111111111111111111111111111111111111")
+    `fp64("0111111111101111111111111111111111111111111111111111111111111111")`
     """
-    def __init__(self, exp_bits=11, frac_bits=52):
-        super().__init__(exp_bits=exp_bits, frac_bits=frac_bits)
+    def __init__(self, exponent_bits=11, fraction_bits=52):
+        super().__init__(exponent_bits=exponent_bits, fraction_bits=fraction_bits)
 
 
 if __name__ == "__main__":
